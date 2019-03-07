9ba07b14575c34644200d13d099a0e7df5531a4d
chaya <collegeps97@gmail.com>
2019-03-06 23:44:36 -0500

    homeowork 5

--- addItem.php
+++ addItem.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+
+
+<?php
+
+
+    if (isset($_SESSION['LoggedIN'])&& $_SESSION['LoggedIN']==true){
+        echo "Welcome "  . $_Cookie['username'];
+    }
+    else{
+        echo "Not logged in";
+        echo "<a href='signup.php'>Log in</a>";
+    }
+?>
--- response.php
+++ response.php
@@ -1,27 +1,31 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
         <?php
         // put your code here
         $_array_of_usernames = array("pessie97@gmail.com", "mordyys@gmail.com","collegeps97@gmail.com");
-        $_email= $_POST['email'];
-        $_password=$_POST['password'];
+         $array_of_passwords = array(12345, 1221, 3232);
         
-        if (array_key_exists($_email, $_array_of_usernames)){
-            header("Location:addItem.php");
+         
+         $_email= $_GET['email'];
+        $_password=$_GET['password'];
+        
+        
+               
+       
+        if (in_array($_email, $_array_of_usernames)){
+            if (in_array($_password, $array_of_passwords)){
+                $_SESSION['LoggedIN']=TRUE;
+                setcookie('username','email', time()+4800);
+                echo "<a href = 'addItem.php'>Add Item</a>";
+            }
+             else{
+            echo "<a href = 'join.php'>Sign Up</a>";
+            $_SESSION['LoggedIN']=FALSE;
+        }
+            
         }
         else{
-            echo "Invalid username or password";
+            echo "<a href = 'join.php'>Sign Up</a>";
+            $_SESSION['LoggedIN']=FALSE;
         }
         ?>
-    </body>
-</html>
+  
