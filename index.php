972ad8571bbe7a324f4ad5052de4f0398a594a3b
chaya <collegeps97@gmail.com>
2019-07-14 16:07:32 -0400

    update for summer class

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <h4>Click here to create your new schedule</h4>
 <h4>Click<a href="signup.php"> here </a>to create your new schedule</h4>
 <h4>Click here to <a href="signup.php">sign in</a> to your schedule</h4>
+<a href ="Activities.php">Here</a>
 
 
 
